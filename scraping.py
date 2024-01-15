3c194486c5ac36248ed390ef868b207bc87ef9c4
Lara Filippone <filipponelara@gmail.com>
2024-01-15 20:47:02 +0100

    clean up from redundant parts

--- scraping.py
+++ scraping.py
@@ -20,24 +20,6 @@ user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 OPR/76.0.4017.177",
 ]
 
-# Randomly choose a user agent
-random_user_agent = random.choice(user_agents)
-
-# Headers used for making HTTP requests to simulate a browser visit
-headers = {
-    "authority": "www.amazon.com",
-    "pragma": "no-cache",
-    "cache-control": "no-cache",
-    "dnt": "1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "sec-fetch-site": "none",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-dest": "document",
-    "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-}
-
 # URLS list for testing (can be removed if URLs are dynamically generated in the GUI script)
 URLS = [
  "https://www.amazon.com/product-reviews/B000063XH7/ref=cm_cr_arp_d_viewopt_srt?ie=UTF8&filterByStar=all_stars&reviewerType=all_reviews&pageNumber=1&sortBy=recent#reviews-filter-bar"
